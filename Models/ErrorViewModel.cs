c8ff9bb7b28deb56342fa47aee8fc910515b034a
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-03-18 13:43:16 -0500

    Restarted Project using Roles, I will be editing the register schtml page to apply dependency injection for use of the roles

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Propertymanagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
